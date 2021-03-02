ec15da75afcc63828351829c9a6901527fc6d355
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2021-03-02 15:07:57 -0300

    Adicionado operadores 3

---
+++ Fundamentos/operadores_3.dart
@@ -0,0 +1,22 @@
+main() {
+
+ int a = 3;
+ int b = 4;
+
+// Operadores Unários
+ a++; // Postfix
+ a--; // Prefix
+
+ print(a);
+
+ print(a++ == --b); // Fuja disso complicado
+ print(a == b);
+
+// Operador lógico unário (Not)
+ print(!true);
+ print(!true);
+
+ bool x = false;
+ print(!x);
+  
+}
