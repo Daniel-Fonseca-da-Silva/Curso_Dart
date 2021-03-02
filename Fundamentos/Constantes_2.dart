19eb1f6fce3d19029e0ad41701cb5a83aec3a8d0
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2021-03-02 17:10:39 -0300

    Adicionando constantes 2

---
+++ Fundamentos/Constantes_2.dart
@@ -0,0 +1,11 @@
+import 'dart:io'; // Permite entrada e saída de dados
+
+main() {
+
+  const lista = ['Nikolai', 'Vladimir', 'Jhosom'];
+  // lista = ['Banana', 'Apple'];
+
+  lista.add('Erick');
+  print(lista);
+  
+}
