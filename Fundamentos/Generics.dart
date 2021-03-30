398cd0f4ea060920bda9888b002d0ecee51c2a6c
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2021-03-30 01:41:18 -0300

    Adicionado arquivo Generics

---
+++ Fundamentos/Generics.dart
@@ -0,0 +1,17 @@
+main () {
+
+  print('Início!');
+
+  List<String> frutas = ['banana', 'mamão', 'abacate'];
+  frutas.add('morango');
+
+  print(frutas);
+
+  Map<String, double> salarios = {
+    'gerente': 19345.78,
+    'vendedor': 16345.80,
+    'estagiario': 600.00,
+  };
+
+  print(salarios);
+}
