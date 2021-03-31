990b8a0c759f301eed2e01ec5aca54c361d67c55
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2021-03-30 23:08:30 -0300

    Adicionado arquivo for1

---
+++ Controle/for1.dart
@@ -0,0 +1,23 @@
+main() {
+
+  for(int a = 1; a <= 10; a += 1)
+  {
+    print('a = $a');
+  }
+
+  print('Fim!');
+
+  for(int a = 100; a >= 0; a -= 4)
+  {
+    print('a = a $a');
+  }
+
+  int b = 0;
+  for(; b <= 10; b++)
+  {
+    print('b = $b');
+  }
+
+  print('b = $b');
+
+}
