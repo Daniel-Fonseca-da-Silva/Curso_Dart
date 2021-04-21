7ecfe152d6f3ca23fe40c24526db3598d2761d91
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2021-04-21 03:16:08 -0300

    Adicionado arquivo classe_data

---
+++ OO/classe_data.dart
@@ -0,0 +1,22 @@
+class Data {
+
+  int dia;
+  int mes;
+  int ano;
+
+}
+
+main() {
+  var dataAniversario = new Data();
+  dataAniversario.dia = 3;
+  dataAniversario.mes = 10;
+  dataAniversario.ano = 2020;
+
+  Data dataCompra = Data();
+  dataCompra.dia = 23;
+  dataCompra.mes = 12;
+  dataCompra.ano = 2021;
+
+  print("${dataAniversario.dia}/${dataAniversario.mes}/${dataAniversario.ano}");
+  print("${dataCompra.dia}/${dataCompra.mes}/${dataCompra.ano}");
+}
