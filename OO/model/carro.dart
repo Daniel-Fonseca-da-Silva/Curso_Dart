d7a428d40d38fa5debbdaa951176989a5f5d88fe
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2021-05-04 13:20:47 -0300

    Adicionado arquivo carro

---
+++ OO/model/carro.dart
@@ -0,0 +1,43 @@
+class Carro {
+
+  final int velocidadeMaxima;
+  int velocidadeAtual = 0;
+  
+  Carro([this.velocidadeMaxima = 180]);
+
+  int acelerar()
+  {
+    // 202 + 5 >= 203
+    if(velocidadeAtual + 5 >= velocidadeMaxima)
+    {
+      velocidadeAtual = velocidadeMaxima;
+    }
+    else
+    {
+      velocidadeAtual += 5;
+    }
+
+    return velocidadeAtual;
+  } 
+  
+  int frear()
+  {
+    // 202 + 5 >= 203
+    if(velocidadeAtual - 5 <= 0)
+    {
+      velocidadeAtual = 0;
+    }
+    else
+    {
+      velocidadeAtual -= 5;
+    }
+
+    return velocidadeAtual;
+  }
+
+  bool estaNoLimite()
+  {
+    return velocidadeAtual == velocidadeMaxima;
+  }
+
+}
